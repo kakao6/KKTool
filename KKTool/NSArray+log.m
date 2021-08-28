3a5650a8ebc3b298dc3c51e6f124a8d371a59c61
weiyueyong <weiyueyong@kuaidihelp.com>
2021-08-28 13:35:12 +0800

    Revert "删除KKTool NSArray+log.h"
    This reverts commit 20978256218295727ee9a3cd6369fcc90bc1b379.

---
+++ KKTool/NSArray+log.m
@@ -0,0 +1,14 @@
+//
+//  NSArray+log.m
+//  KKTool
+//
+//  Created by 魏跃勇 on 2021/8/15.
+//
+
+#import "NSArray+log.h"
+
+@implementation NSArray (log)
+- (void)log {
+    NSLog(@"array");
+}
+@end
